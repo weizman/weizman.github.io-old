b0814e309e3928fda30ca549c99da721cff68fd0
weizman <weizmangal@gmail.com>
2021-08-02 02:41:44 +0300

    add close sidenav button

--- content/js/main.js
+++ content/js/main.js
@@ -3,12 +3,41 @@ var converter = new showdown.Converter();
 var URL_BASE = 'https://weizman.github.io/';
 var URL_PATH = location.search.replace('?', '');
 
-if (location.origin === 'http://localhost') {
+if (location.origin.split(':')[1] === '//localhost') {
   // for localhost debugging
-  var URL_BASE = location.origin + location.pathname;
+  URL_BASE = location.origin + location.pathname;
 }
 
+let sidenavOpen = true;
+
 var container;
+
+
+function hasClass(el, className)
+{
+  if (el.classList)
+    return el.classList.contains(className);
+  return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+}
+
+function addClass(el, className)
+{
+  if (el.classList)
+    el.classList.add(className)
+  else if (!hasClass(el, className))
+    el.className += " " + className;
+}
+
+function removeClass(el, className)
+{
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className))
+  {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+    el.className = el.className.replace(reg, ' ');
+  }
+}
 
 function get(url, onload, onerror) {
   var xhr = new XMLHttpRequest();
@@ -44,4 +73,20 @@ function present(file) {
 
 function setContainer(element) {
   container = element;
+}
+
+function handleSideNav(hide, sidenav, sidecontent, main) {
+  if (sidenavOpen) {
+    sidenav.setAttribute('style', 'width: 50px;');
+    sidecontent.setAttribute('style', 'display: none;');
+    main.setAttribute('style', 'margin-left: 50px;');
+    addClass(hide, 'closed');
+    sidenavOpen = false;
+  } else {
+    sidenav.removeAttribute('style');
+    sidecontent.removeAttribute('style');
+    main.removeAttribute('style');
+    removeClass(hide, 'closed');
+    sidenavOpen = true;
+  }
 }
